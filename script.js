5648897e9e27d6153734407a602f80bc8d09adb5
Souvik <souvikroy1999ab@gmail.com>
2023-09-01 09:27:40 +0530

    refractored the code

--- script.js
+++ script.js
@@ -1,93 +1,65 @@
 import { BASE_URL } from './config.js';
 import formattedDate from './utils.js';
 
-let centers = [];
 const cards = document.querySelector('.cards');
 const searchBtn = document.querySelector('.searchBox').querySelector('button');
 
-function cowinData(pincode) {
+const cowinData = (pincode) => {
   if (pincode.length !== 6) {
     alert('Enter a valid 6-digit pincode');
     return;
   }
 
-  let url = `${BASE_URL}/findByPin?pincode=${pincode}&date=${formattedDate()}`;
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onload = function () {
-    if (this.status === 200) {
-      let data = JSON.parse(this.responseText);
+  const url = `${BASE_URL}/findByPin?pincode=${pincode}&date=${formattedDate()}`;
 
-      if (data.sessions !== []) {
-        data.sessions.map((e, i) => {
-          let centerInfo = [
-            e.name,
-            e.address,
-            e.vaccine,
-            e.date,
-            e.min_age_limit,
-            e.available_capacity,
-            e.block_name,
-            e.district_name,
-            e.slots,
-          ];
-          centers.push(centerInfo);
-          let code = `
-      <div class="card">
-      <h1>
-      <span class="category">Center Name - </span>
-      ${centers[i][0]}
-    </h1>
-    <div class="innerCard">
-    <h3>
-    <span class="category">Center Address - </span>
-    ${centers[i][1]}
-  </h3>
-  <h3>
-    <span class="category">Vaccine Name - </span>
-    ${centers[i][2]}
-  </h3>
-  <h3>
-    <span class="category">Date Of Vaccination - </span>
-    ${centers[i][3]}
-  </h3>
-  <h3>
-    <span class="category">Minimum Age Limit - </span>
-    ${centers[i][4]}
-  </h3>
-  <h3>
-    <span class="category">Available Capacity - </span>
-    ${centers[i][5]}
-  </h3>
-  <h3>
-    <span class="category">Block Name - </span>
-    ${centers[i][6]}
-  </h3>
-  <h3>
-    <span class="category">District Name - </span>
-    ${centers[i][7]}
-  </h3>
-  <h3>
-    <span class="category">Available Slots - </span>
-     ${centers[i][8].join(' | ')}
-  </h3>
-    </div>
-    </div>`;
-          cards.innerHTML += code;
-        });
-        // console.log(data.sessions.length);
-        if (data.sessions.length === 0) {
-          alert('No Vaccinations Available');
-        }
-        centers = [];
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-    } else {
-      alert('Some error occured');
-    }
-  };
+      return response.json();
+    })
+    .then((data) => {
+      if (data.sessions && data.sessions.length > 0) {
+        let content = data.sessions
+          .map((session) => {
+            const details = [
+              ['Center Address', session.address],
+              ['Vaccine Name', session.vaccine],
+              ['Date Of Vaccination', session.date],
+              ['Minimum Age Limit', session.min_age_limit],
+              ['Available Capacity', session.available_capacity],
+              ['Block Name', session.block_name],
+              ['District Name', session.district_name],
+              ['Available Slots', session.slots.join(' | ')],
+            ];
 
-  xhr.send();
-}
+            const detailsHTML = details
+              .map(
+                ([label, value]) =>
+                  `<h3><span class="category">${label} - </span>${value}</h3>`
+              )
+              .join('');
+
+            return `
+            <div class="card">
+              <h1><span class="category">Center Name - </span>${session.name}</h1>
+              <div class="innerCard">
+                ${detailsHTML}
+              </div>
+            </div>`;
+          })
+          .join('');
+
+        cards.innerHTML = content;
+      } else {
+        alert('No Vaccinations Available');
+      }
+    })
+    .catch(() => {
+      alert('Some error occurred');
+    });
+};
 
 const input = document.querySelector('#input');
 input.addEventListener('keypress', (e) => {
